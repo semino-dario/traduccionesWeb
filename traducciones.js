0d1b7317a8eaac8d7919072c1f755b096589ddc5
Dario Semino <orbislibriscontacto@gmail.com>
2022-07-16 23:14:47 -0300

    Commit 1

---
+++ traducciones.js
@@ -0,0 +1,58 @@
+/////////// BOTONES DE IDIOMAS DESACTIVADOS//////////////
+
+let botonFrances = document.querySelector(".boton-frances");
+let botonCastellano1 = document.querySelector(".castellano1");
+let botonIngles = document.querySelector(".boton-ingles");
+let botonCastellano2 = document.querySelector(".castellano2");
+
+botonIngles.disabled = true;
+botonCastellano1.disabled = true;
+botonCastellano2.disabled = true;
+botonFrances.disabled= true;
+
+///////EFECTOS DE FADE IN E INGRESOS DE ELMENTOS A MEDIDA QUE SE SCROLLEA////////
+
+let divIdiomas = document.querySelector(".divIdiomas")
+let animado = document.querySelectorAll(".animado")
+let flaubert = document.querySelector(".flaubert")
+let cotizacion = document.querySelector(".cardcotiza")
+let contacto = document.querySelector(".cardContacto")
+let imagenPortada = document.querySelector(".imagen-portada")
+
+function mostrarScroll (){
+                          let scrollTop = document.documentElement.scrollTop;
+                          let alturaFlaubert = flaubert.offsetTop;
+                          let alturaCotizacion = cotizacion.offsetTop;
+                          let alturaContacto = contacto.offsetTop;
+                          let alturaBotonFrances = botonFrances.offsetTop;
+                            for (let i=0; i<animado.length; i++)
+                            { let alturaAnimado = animado[i].offsetTop;
+                              if (alturaAnimado - 630 < scrollTop)
+                                {animado[i].style.opacity = 1;
+                                      animado[i].classList.add("mostrarArriba")
+                              }
+
+                              if (alturaFlaubert - 500 < scrollTop)
+                              {
+                              flaubert.style.opacity = 1;
+
+                              }
+                              if (alturaCotizacion -700 < scrollTop)
+                              {
+                                cotizacion.style.opacity = 1;
+                                cotizacion.classList.add("mostrarDerecha")
+                              }
+                              if (alturaContacto - 700 < scrollTop)
+                              { contacto.style.opacity = 1;
+                                contacto.classList.add("mostrarIzquierda")
+                              }
+                              if (alturaAnimado - 1800 < scrollTop){
+                                divIdiomas.style.opacity = 1;
+                              }
+                            }
+                          }
+
+function aparecer() {imagenPortada.style.opacity = 1}
+aparecer()
+
+window.addEventListener("scroll", mostrarScroll)
